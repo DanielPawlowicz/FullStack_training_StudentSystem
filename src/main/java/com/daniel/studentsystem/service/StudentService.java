ed68a76b4ffe07c6c5618a3650db0306e33f6335
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-03-08 16:49:04 +0100

    Getting data from the database

--- src/main/java/com/daniel/studentsystem/service/StudentService.java
+++ src/main/java/com/daniel/studentsystem/service/StudentService.java
@@ -2,7 +2,10 @@ package com.daniel.studentsystem.service;
 
 import com.daniel.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
 
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
