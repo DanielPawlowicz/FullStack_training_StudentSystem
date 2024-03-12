7063e60b786719f4e014d3921b2dd58f43ef768c
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-03-12 10:12:39 +0100

    Full app

--- src/main/java/com/daniel/studentsystem/controller/StudentController.java
+++ src/main/java/com/daniel/studentsystem/controller/StudentController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin // allows to fetch
 public class StudentController {
     @Autowired
     private StudentService studentService;
